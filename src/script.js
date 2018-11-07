9c27ff19f75cfc2c05a69094854da0e60976101c
Alex Graves <graves@thedp.com>
2018-11-07 17:14:07 -0500

    write bounding box function, add debug mode

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@
 
 const SUPPORTS_POINTER = 'PointerEvent' in window;
 const SUPPORTS_TOUCH = 'ontouchstart' in window;
+var debugMode = false;
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
@@ -35,6 +36,13 @@ this.polygons = [];
 this.polygonIndex = 0;
 
 /* CLASSES */
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
 class Circle {
   constructor(x, y, r) {
     this.x = x;
@@ -47,6 +55,45 @@ class Polygon {
   constructor(edges, color) {
     this.edges = edges;
     this.color = color;
+  }
+
+  boundingBox() {
+    this.boundaries = [
+      new Point(0, 0), // top left
+      new Point(0, 0), // top right
+      new Point(0, 0), // bottom right
+      new Point(0, 0)  // bottom left
+    ];
+    const edges = this.edges;
+
+    // sort edges array on x
+    edges.sort((a, b) => a.x === b.x ? 0 : a.x < b.x ? -1 : 1);
+    this.boundaries[0].x = edges[0].x;
+    this.boundaries[1].x = edges[edges.length - 1].x;
+    this.boundaries[2].x = edges[edges.length - 1].x;
+    this.boundaries[3].x = edges[0].x;
+    const width = edges[edges.length - 1].x - edges[0].x;
+
+    // sort edges array on y
+    edges.sort((a, b) => a.y === b.y ? 0 : a.y < b.y ? -1 : 1);
+    this.boundaries[0].y = edges[0].y;
+    this.boundaries[1].y = edges[0].y;
+    this.boundaries[2].y = edges[edges.length - 1].y;
+    this.boundaries[3].y = edges[edges.length - 1].y;
+    const height = edges[edges.length - 1].y - edges[0].y;
+
+    // DEBUG MODE: draw bounding box
+    if (debugMode) {
+      context.lineWidth = 1;
+      context.strokeStyle = '#FF0000';
+      context.strokeRect(this.boundaries[0].x, this.boundaries[0].y, width, height);
+      context.lineWidth = this.currentSize;
+    }
+  }
+
+  scatterPoints() {
+    // scatter points using stratified sampling within bounding box
+    // delete points if they're not within the shape
   }
 }
 
@@ -129,19 +176,19 @@ function detectEdges(circle, color) {
     n = context.getImageData(currX++, y, 1, 1).data;
   }
 
-  const start = {x: x - 1, y: y};
+  const start = new Point(x - 1, y);
 
   // recursive function to determine a polygon's edges
   const addEdge = (point, prev) => {
     const neighbors = [
-      {x: point.x + 1, y: point.y    },
-      {x: point.x + 1, y: point.y + 1},
-      {x: point.x,     y: point.y + 1},
-      {x: point.x - 1, y: point.y + 1},
-      {x: point.x - 1, y: point.y    },
-      {x: point.x - 1, y: point.y - 1},
-      {x: point.x,     y: point.y - 1},
-      {x: point.x + 1, y: point.y - 1}
+      new Point(point.x + 1, point.y    ),
+      new Point(point.x + 1, point.y + 1),
+      new Point(point.x,     point.y + 1),
+      new Point(point.x - 1, point.y + 1),
+      new Point(point.x - 1, point.y    ),
+      new Point(point.x - 1, point.y - 1),
+      new Point(point.x,     point.y - 1),
+      new Point(point.x + 1, point.y - 1)
     ];
 
     // order neighbors so that it starts right after prev
@@ -170,15 +217,11 @@ function detectEdges(circle, color) {
     });
 
     if (firstNot !== -1) {
-      edges.push({x: point.x, y: point.y});
+      edges.push(new Point(point.x, point.y));
       const next = neighbors[firstSame];
       // continue until we reach the original starting point
-      if (next.x === start.x && next.y === start.y) {
-        console.log('back to start!');
-        return;
-      } else {
-        return addEdge(next, point);
-      }
+      if (next.x === start.x && next.y === start.y) return;
+      return addEdge(next, point);
     }
   }
 
@@ -187,7 +230,6 @@ function detectEdges(circle, color) {
 }
 
 function generateCity() {
-  // console.log(this.clickData);
   const i = this.clickData[this.currentBrush].length - 1;
   this.polygons.push(detectEdges(this.clickData[this.currentBrush][i], this.currentBrush));
 }
@@ -231,6 +273,10 @@ brushSize.addEventListener('input', event => {
   currentBrushSize.innerText = brushSize.value;
 });
 
+document.getElementById('debug').addEventListener('change', event => {
+  debugMode = event.target.checked;
+});
+
 document.getElementById('generate').addEventListener('click', event => {
   // TODO: currently, this just runs edge detection on the last-drawn point
   generateCity();
@@ -239,7 +285,9 @@ document.getElementById('generate').addEventListener('click', event => {
 document.getElementById('edges').addEventListener('click', event => {
   // debug feature; see edges created from detection algorithm
   for (let i = this.polygonIndex; i < this.polygons.length; i++) {
-    for (point of this.polygons[i].edges) {
+    // generate bounding box
+    this.polygons[i].boundingBox();
+    for (let point of this.polygons[i].edges) {
       context.fillStyle = '#000';
       context.fillRect(point.x, point.y, 1, 1);
     }
